f32d7ec302229ba0112a99d603877e43dff5adf4
rodolphe2006 <rodolphebahati@gmail.com>
2025-08-06 15:08:58 +0300

    just added the logo

--- src/Components/topnav/Topnav.tsx
+++ src/Components/topnav/Topnav.tsx
@@ -5,6 +5,9 @@ import Categories from "../categories/Categories";
 function Topnav() {
   return (
     <nav className="navtop">
+      <div className="Logo">
+        <h1>Tech-Market</h1>
+      </div>
       <Search />
       <Categories />
     </nav>
